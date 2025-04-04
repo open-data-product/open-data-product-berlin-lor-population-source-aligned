ac35c9596d1eb154043631b57d72b63be74cf1ce
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-04-04 18:43:33 +0200

    chore: add click

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import getopt
 import os
 import sys
 
+import click
+
 from config.data_transformation_gold_loader import load_data_transformation_gold
 from config.data_transformation_silver_loader import load_data_transformation_silver
 from config.odps_loader import load_odps
@@ -19,32 +21,10 @@ file_path = os.path.realpath(__file__)
 script_path = os.path.dirname(file_path)
 
 
-@TrackingDecorator.track_time
-def main(argv):
-    # Set default values
-    clean = False
-    quiet = False
-
-    # Read command line arguments
-    try:
-        opts, args = getopt.getopt(argv, "hcq", ["help", "clean", "quiet"])
-    except getopt.GetoptError:
-        print("main.py --help --clean --quiet")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print("main.py")
-            print("--help                           show this help")
-            print(
-                "--clean                          clean intermediate results before start"
-            )
-            print("--quiet                          do not log outputs")
-            sys.exit()
-        elif opt in ("-c", "--clean"):
-            clean = True
-        elif opt in ("-q", "--quiet"):
-            quiet = True
-
+@click.command()
+@click.option("--clean", default=False, help="Regenerate results.")
+@click.option("--quiet", default=False, help="Do not log outputs.")
+def main(clean, quiet):
     data_path = os.path.join(script_path, "data")
     bronze_path = os.path.join(data_path, "01-bronze")
     silver_path = os.path.join(data_path, "02-silver")
